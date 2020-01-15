3c01a0e3c9f68e500931c60120b1a435a169ea90
Azzimandias Shar <azzimandias@gmail.com>
2020-01-16 02:29:08 +0300

    SingUp

--- DB.py
+++ DB.py
@@ -3,8 +3,8 @@ import psycopg2
 # подключение к базе данных
 con = psycopg2.connect(host="",
                        database="",
-                       user="",
-                       password="")
+                       user="postgres",
+                       password="fuckyou")
 
 # указатель
 cur = con.cursor()
